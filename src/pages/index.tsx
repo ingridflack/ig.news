ce89539be8230684d6b985ae1c0ebc30560ccfd6
ingridflack <ingrid.flack@newtail.com.br>
2022-02-24 19:03:51 -0300

    Home page created and styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
-// import styles from "../styles/home.module.scss";
 import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          width={334}
+          height={520}
+        />
+      </main>
     </>
   );
 }
